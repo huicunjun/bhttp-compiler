8a45ba3a4c5bac3f341ce976c60ae82c5a203de9
威威君” <1214585092@qq.com>
2020-06-01 19:42:23 +0800

    单独剥离注解库

--- app/src/main/java/com/ldw/test/bhttp_compiler_test/MainActivity.java
+++ app/src/main/java/com/ldw/test/bhttp_compiler_test/MainActivity.java
@@ -4,14 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.bhttp.wrapper.generator.BHttp;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
